1869a61ca75da9e5f812801967848a4d0f3029ac
john-odonnell <johnodonnell77@my.uri.edu>
2019-09-26 04:57:48 +0000

    commented main_1.cpp through main_5.cpp

--- assignment_01/main_1.cpp
+++ assignment_01/main_1.cpp
@@ -2,15 +2,18 @@
 #include <iomanip>
 
 int main () {
-    int a, b, c;
+    int a, b, c;    // instantiate a b and c
 
+    // push strings to output stream
     std::cout << "~~Addition Calculator~~" << std::endl;
     std::cout << "Enter two numbers to add: " << std::ends;
 
+    // push the input stream to a and b
     std::cin >> a >> b;
 
-    c = a + b;
+    c = a + b;  // addition
 
+    // push addition results to output stream
     std::cout << a << " + " << b << " = " << c << std::endl;
 
     return 0;
--- assignment_01/main_3.cpp
+++ assignment_01/main_3.cpp
@@ -2,15 +2,18 @@
 #include <iomanip>
 
 int main () {
-    int a, b, c;
+    int a, b, c;    // instantiate a b and c
 
+    // push strings to the output stream
     std::cout << "~~Division Calculator~~" << std::endl;
     std::cout << "Enter two numbers to divide: " << std::ends;
 
+    // push input stream to a and b
     std::cin >> a >> b;
 
-    c = a / b;
+    c = a / b;  // division
 
+    // push division results to output stream
     std::cout << a << " / " << b << " = " << c << std::endl;
 
     return 0;
