f5ebd0f116b4d8de8fad8eb06fa89fef67661742
john-odonnell <johnodonnell77@my.uri.edu>
2019-10-12 01:58:29 +0000

    not working: 4, 17

--- assignment_02/main_9.cpp
+++ assignment_02/main_9.cpp
@@ -14,16 +14,16 @@ void draw_triangle_top(int base, int max, int increment) {
 
 void draw_triangle_bottom(int base, int max, int increment) {
     int start_here = base;
-    for (int i = base; base < max; i++) {
-        base += increment;
-        start_here++;
+    for (int i = base; start_here + increment - 1 < max; i++) {
+        start_here += increment;
     }
     while (start_here >= base) {
         for (int i = 0; i < start_here; i++) {
             std::cout << "+";
         }
         std::cout << std::endl;
-        max -= increment;
+
+        start_here -= increment;
     }
 }
 
